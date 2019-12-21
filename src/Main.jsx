ef41607738963e208fc61ad12ef4288dc8d86f40
tai <thanh-tai.truong@ekino.com>
2019-12-21 15:14:58 +0700

    fix

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import ProductList from "./components/ProductList/ProductList";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Layout from "./components/Layout/Layout";
 import dataJSON from './data.json'
 import Cart from "./components/Cart/Cart";
-import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
-import NotFound from './components/NotFound/NotFound'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css'
-import ProductDetail  from "./components/ProductDetail/ProductDetail";
+
+import Loading from './components/Loading/Loading'
+
+const Login = React.lazy(() => import("./components/Login/Login"));
+const ProductList = React.lazy(() => import("./components/ProductList/ProductList"));
+const NotFound = React.lazy(() => import('./components/NotFound/NotFound'))
+const Register = React.lazy(() => import("./components/Register/Register"))
+const ProductDetail  = React.lazy(() => import("./components/ProductDetail/ProductDetail"))
 
 /*
 STORE   => createContext()
@@ -64,39 +67,42 @@ function Main() {
       <Header addedProductsLength={addedProducts.length}>
         <Cart products={addedProducts}/>
       </Header>
-      <Switch>
-        <Route path="/" exact>
-          <Layout>
-            <ProductList data={dataJSON.data} onClickProduct={callbackOfParent}/>
-            <Sidebar />
-          </Layout>
-        </Route>
-        <Route 
-          path="/(login|dang-nhap)" 
-          render={(props) => {
-            // if(props.isLogin) {
-            //   return <Login />
-            // }
-            // return <NotFound />
-            return <Login />
-          }} 
-        />
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route
-          path="/product-detail/:id" 
-          // render={(props) => {
-          //   console.log(props.match.params.id)
-          //   return <ProductDetail id={props.match.params.id}/>
-          // }}
-        >
-          <ProductDetail />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <React.Suspense fallback={<Loading />}>
+        <Switch>
+          <Route path="/" exact>
+            <Layout>
+              <ProductList data={dataJSON.data} onClickProduct={callbackOfParent}/>
+              <Sidebar />
+            </Layout>
+          </Route>
+          <Route 
+            path="/(login|dang-nhap)" 
+            render={(props) => {
+              // if(props.isLogin) {
+              //   return <Login />
+              // }
+              // return <NotFound />
+              return <Login />
+            }} 
+          />
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route
+            path="/product-detail/:id" 
+            // render={(props) => {
+            //   console.log(props.match.params.id)
+            //   return <ProductDetail id={props.match.params.id}/>
+            // }}
+          >
+            <ProductDetail />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </React.Suspense>
+      
       <Footer />
     </Router>
   );
